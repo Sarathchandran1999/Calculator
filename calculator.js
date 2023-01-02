436aff3862bc77fa57daa5bc92058249c2b1e41d
Sarathchandran1999 <119933158+Sarathchandran1999@users.noreply.github.com>
2023-01-02 14:59:58 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,13 @@
+function buttonclick(val){
+    console.log(val);
+    document.getElementById("screen").value+=val;
+}
+function cclear(){
+    document.getElementById("screen").value="";
+}
+function equalclick(){
+    var text=document.getElementById("screen").value;
+    var result=eval(text);
+    document.getElementById("screen").value=result;
+
+}
